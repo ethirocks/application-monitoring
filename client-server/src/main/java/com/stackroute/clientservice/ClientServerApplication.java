66e38c31d0bb9bd42ad9577d7e79c31f9fa9a4b9
Ayush Bhatia <ayush2850@gmail.com>
2018-11-21 14:54:40 +0530

    After solving issues

--- client-server/src/main/java/com/stackroute/clientservice/ClientServerApplication.java
+++ client-server/src/main/java/com/stackroute/clientservice/ClientServerApplication.java
@@ -1,16 +1,16 @@
-package com.example.hello;
+package com.stackroute.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.stackroute.agent,com.example.hello")
+@ComponentScan(basePackages = "com.stackroute.agent,com.stackroute.clientserver")
 
-public class HelloApplication {
+public class ClientServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
+		SpringApplication.run(ClientServerApplication.class, args);
 		System.out.println("Hello");
 	}
 }
