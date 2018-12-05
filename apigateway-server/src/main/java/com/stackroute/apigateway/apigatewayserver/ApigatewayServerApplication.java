ff763a81c4fa791903006f1c913fa78b8f226309
Ayush Bhatia <ayush2850@gmail.com>
2018-12-05 15:45:43 +0530

    Eureka dependency removed

--- apigateway-server/src/main/java/com/stackroute/apigateway/apigatewayserver/ApigatewayServerApplication.java
+++ apigateway-server/src/main/java/com/stackroute/apigateway/apigatewayserver/ApigatewayServerApplication.java
@@ -2,22 +2,16 @@ package com.stackroute.apigateway.apigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 @EnableZuulProxy
 @SpringBootApplication
 //@EnableDiscoveryClient
