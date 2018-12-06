482153cf82ac94582ac23275c9d118cdbd7c6225
Ayush Bhatia <ayush2850@gmail.com>
2018-12-06 11:29:53 +0530

    api gateway filter added

--- apigateway-server/src/main/java/com/stackroute/apigateway/apigatewayserver/ApigatewayServerApplication.java
+++ apigateway-server/src/main/java/com/stackroute/apigateway/apigatewayserver/ApigatewayServerApplication.java
@@ -1,16 +1,13 @@
 package com.stackroute.apigateway.apigatewayserver;
 
+import com.stackroute.apigateway.apigatewayserver.filter.ErrorFilter;
+import com.stackroute.apigateway.apigatewayserver.filter.PostFilter;
+import com.stackroute.apigateway.apigatewayserver.filter.PreFilter;
+import com.stackroute.apigateway.apigatewayserver.filter.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -22,31 +19,21 @@ public class ApigatewayServerApplication {
 		SpringApplication.run(ApigatewayServerApplication.class, args);
 	}
 	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat =
-				new TomcatServletWebServerFactory() {
-					@Override
-					protected void postProcessContext(Context context) {
-						SecurityConstraint securityConstraint = new SecurityConstraint();
-						securityConstraint.setUserConstraint("CONFIDENTIAL");
-						SecurityCollection collection = new SecurityCollection();
-						collection.addPattern("/*");
-						securityConstraint.addCollection(collection);
-						context.addConstraint(securityConstraint);
-					}
-				};
-		tomcat.addAdditionalTomcatConnectors(redirectConnector());
-		return tomcat;
+	public PreFilter preFilter() {
+		return new PreFilter();
 	}
-
-	//Comment below code if any token error arrises
-	private Connector redirectConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("https");
-		connector.setPort(9004);
-		connector.setSecure(false);
-		connector.setRedirectPort(9005);
-		return connector;
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 
 }
+
