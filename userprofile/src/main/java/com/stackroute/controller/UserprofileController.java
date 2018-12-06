ea0b59c3952aa753f85c244ce74aba0c885c2c27
tanusha.moolammagaari <tanusha.moolammagaari@cgi.com>
2018-12-06 20:51:05 +0530

    cross origin added in user profile

--- userprofile/src/main/java/com/stackroute/controller/UserprofileController.java
+++ userprofile/src/main/java/com/stackroute/controller/UserprofileController.java
@@ -8,14 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class UserprofileController {
 
 
